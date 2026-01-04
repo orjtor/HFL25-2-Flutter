d8c675a8a104f07f1160d76f7fce43092a019274
orjtor <orjan.torang@gmail.com>
2026-01-04 14:44:06 +0100

    v05 flutter pub add firebase_analytics

--- v05_firebase/macos/Flutter/GeneratedPluginRegistrant.swift
+++ v05_firebase/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_analytics
 import firebase_auth
 import firebase_core
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
 }
